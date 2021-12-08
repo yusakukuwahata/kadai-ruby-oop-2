6a3e9cc275a7d98a4c87edce56c6c0e6fdff12f8
yusaku.kuwahata <xyusaku.kuwahata@gmail.com>
2021-12-08 06:44:24 +0000

    fix review

--- mentor.rb
+++ mentor.rb
@@ -1,8 +1,8 @@
 class Mentor
   attr_accessor :name
   
-  def initialize
-    self.name = "煌木"
+  def initialize(name)
+    self.name = name
   end 
   
   def job 
@@ -13,18 +13,14 @@ end
 
 class RailsMentor < Mentor
   
-  def initialize
-    self.name = "赤出"
-  end
-  
   def job 
     puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
   end
   
 end
 
-kirameki = Mentor.new
-akaide = RailsMentor.new
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
 
 kirameki.job
 akaide.job
